fdb4766472ca490dcbb29be9481e2af20eebbf70
yogiyu1002 <yu.xiaoj@northeastern.edu>
2025-04-14 20:26:47 -0700

    quiz questions operations with Mongo working

---
+++ Kambaz/Quizzes/schema.js
@@ -0,0 +1,87 @@
+import mongoose from "mongoose";
+
+
+const ChoiceSchema = new mongoose.Schema({
+    _id: String,
+    title: String,
+});
+
+const QuestionSchema = new mongoose.Schema({
+    _id: String,
+    title: String,
+    type:{
+        type: String,
+        enum: ["multipleChoice", "trueFalse", "fillInTheBlank"],
+        default: "multipleChoice",
+        required: true,
+    },
+    points: Number,
+    question: String,
+    choices:{
+        type: [ChoiceSchema],
+        default: [],
+        required: function() {
+            return this.type === "multipleChoice";
+        },
+    },
+    answer: {
+        type: String,
+        required: function() {
+            return this.type === "multipleChoice" || this.type === "trueFalse";
+        },
+    },
+    answers:{
+        type: [String],
+        default: [],
+        required: function() {
+            return this.type === "fillInTheBlank";
+        },
+    }
+});
+
+
+
+const quizSchema = new mongoose.Schema({
+    _id: String,
+    title: String,
+    isPublished: { type: Boolean, default: false },
+    type: {
+      type: String,
+      enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"],
+      default: "Graded Quiz",
+    },
+    points: { type: Number, default: 0 },
+    group: { type: String, default: "Quizzes" },
+    shuffle: { type: Boolean, default: true },
+    timeLimit: { type: Number, default: 0 },
+    multipleAttempts: { type: Boolean, default: false },
+    numberAttempts: { type: Number, default: 1 },
+    showAnswersImmediately: { type: Boolean, default: true },
+    showAnswersAfter: { type: String, default: "" },
+    accessCode: { type: String, default: "" },
+    oneQuestionAtATime: { type: Boolean, default: true },
+    webcam: { type: Boolean, default: false },
+    lock: { type: Boolean, default: false },
+    dueDate: {
+        type: String,
+        required: false,
+        default: ""
+    },
+    availableDate:{
+        type: String,
+        default: "",
+        required: false
+    },
+    untildate:{
+        type: String,
+        default: "",
+        required: false
+    },
+    questions: {
+        type: [QuestionSchema],
+        default: [],
+        required: false,
+    },
+    course: { type: String, ref: "CourseModel" },
+}, { collection: "quizzes" });
+export default quizSchema;
