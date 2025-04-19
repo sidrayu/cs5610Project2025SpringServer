74c34363e84a49c6708ebdd9fe8b316740f6bbd4
Sichun Teng <sichun.teng@gmail.com>
2025-04-19 16:47:44 -0700

    Quiz list and quiz detailed page connected with backend mongo

--- Kambaz/Quizzes/dao.js
+++ Kambaz/Quizzes/dao.js
@@ -1,24 +1,45 @@
 import model from "./model.js";
 import { v4 as uuidv4 } from 'uuid';
 
-
+// Get all quizzes
 export const allQuizzes = () => {
     return model.find();
-}
+};
 
+// Get quizzes by course ID
+export const findQuizzesByCourseId = (courseId) => {
+    return model.find({ course: courseId });
+};
+
+// Get quiz by ID
 export const findQuizById = (quizId) => {
-    return model.findById(quizId);
-}
+    return model.findOne({ _id: quizId });
+};
 
+// Create a new quiz
+export const createQuiz = (quiz) => {
+    quiz._id = quiz._id || uuidv4();
+    return model.create(quiz);
+};
+
+// Update an existing quiz
 export const updateQuizById = (quizId, quiz) => {
     console.log("Updating quiz with ID:", quizId);
     console.log("New quiz data:", quiz);
     return model.updateOne({ _id: quizId }, { $set: quiz });
-}
+};
 
-export const createQuiz = (quiz) => {
-    quiz._id = uuidv4();
-    return model.create(quiz);
-}
+// Delete a quiz
+export const deleteQuiz = (quizId) => {
+    return model.deleteOne({ _id: quizId });
+};
+
+// Toggle quiz publish status
+export const toggleQuizPublish = (quizId, isPublished) => {
+    return model.updateOne(
+        { _id: quizId },
+        { $set: { isPublished } }
+    );
+};
 
 
--- Kambaz/Quizzes/routes.js
+++ Kambaz/Quizzes/routes.js
@@ -1,28 +1,59 @@
 import * as dao from './dao.js';
 export default function QuizRoutes(app) {
 
-    app.put("/api/courses/:courseId/quizzes/:quizId", async (req, res) => {
-        const { quizId } = req.params;
-        const updateQuiz = {
-                ...req.body,
-            }
-        const updatedQuiz = await dao.updateQuizById(quizId, updateQuiz);
-        res.json(updatedQuiz);
-        }
-    );
+    // Get all quizzes
+    app.get("/api/quizzes/getAll", async (req, res) => {
+        const quizzes = await dao.allQuizzes();
+        res.json(quizzes);
+    });
+
+    // Get quizzes by course ID
+    app.get("/api/courses/:courseId/quizzes", async (req, res) => {
+        const { courseId } = req.params;
+        const quizzes = await dao.findQuizzesByCourseId(courseId);
+        res.json(quizzes);
+    });
+
+    // Get quiz by ID
     app.get("/api/courses/:courseId/quizzes/:quizId", async (req, res) => {
         const { quizId } = req.params;
         const quiz = await dao.findQuizById(quizId);
         res.json(quiz);
-        
-        }
-    );
-    app.get("/api/quizzes/getAll", async (req, res) => {
-        
-        const quizzes = await dao.allQuizzes();
-        res.json(quizzes);
-        
-        }
-    );
+    });
 
+    // Create a new quiz
+    app.post("/api/courses/:courseId/quizzes", async (req, res) => {
+        const { courseId } = req.params;
+        const newQuiz = {
+            ...req.body,
+            course: courseId
+        };
+        const createdQuiz = await dao.createQuiz(newQuiz);
+        res.json(createdQuiz);
+    });
+
+    // Update an existing quiz
+    app.put("/api/courses/:courseId/quizzes/:quizId", async (req, res) => {
+        const { quizId } = req.params;
+        const updateQuiz = {
+            ...req.body,
+        };
+        const updatedQuiz = await dao.updateQuizById(quizId, updateQuiz);
+        res.json(updatedQuiz);
+    });
+
+    // Delete a quiz
+    app.delete("/api/courses/:courseId/quizzes/:quizId", async (req, res) => {
+        const { quizId } = req.params;
+        const status = await dao.deleteQuiz(quizId);
+        res.json(status);
+    });
+
+    // Toggle quiz publish status
+    app.put("/api/courses/:courseId/quizzes/:quizId/publish", async (req, res) => {
+        const { quizId } = req.params;
+        const { isPublished } = req.body;
+        const updatedQuiz = await dao.toggleQuizPublish(quizId, isPublished);
+        res.json(updatedQuiz);
+    });
 }
